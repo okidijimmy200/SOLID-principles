837f443516d8248be90f43ac5433ad0f11cfcfc7
okidijimmy200 <okidijimmie@gmail.com>
2022-09-14 11:34:01 +0300

    test db complete

--- db.py
+++ db.py
@@ -66,7 +66,7 @@ class MemorySystem(DatabaseBInterface):
     def create(self, location: str, data: Dict[str, str]) -> Tuple[bool, str]:
         print(f"Creating data in location {location}")
         try:
-            with open(f"{self.base_path}/{location}.json", "w") as f:
+            with open(f"{location}", "w") as f:
                 json.dump(data, f)
             reason = f"Data created successfully in location {location}"
             print(reason)
@@ -83,7 +83,7 @@ class MemorySystem(DatabaseBInterface):
 
     def read(self, location: str) -> Tuple[bool, str, Dict[str, str]]:
         try:
-            f= open(f"{self.base_path}/{location}.json")
+            f= open(f"{location}")
             data = json.load(f)
             reason =f"Data read from {location}"
             return True, reason, data
@@ -93,15 +93,15 @@ class MemorySystem(DatabaseBInterface):
 
     def update(self, location: str, data: Dict[str, str]) -> Tuple[bool, str]:
         try:
-            f= open(f"{self.base_path}/{location}.json")
+            f= open(f"{location}")
             old_data = json.load(f)
             # merge json files
             z = dict(list(old_data.items()) + list(data.items()))
 
-            with open(f"{self.base_path}/{location}.json", "w") as f:
+            with open(f"{location}", "w") as f:
                 json.dump(z, f)
 
-            reason = f"Data updated successfully in location {location}"
+            reason = f"Data updated successfully in location"
             print(reason)
             return True, reason
         except  BaseException as err:
@@ -110,8 +110,8 @@ class MemorySystem(DatabaseBInterface):
 
     def delete(self, location: str) -> Tuple[bool, str]:
         try:
-            if os.path.exists(f"{self.base_path}/{location}.json"):
-                os.remove(f"{self.base_path}/{location}.json")
+            if os.path.exists(f"{location}"):
+                os.remove(f"{location}")
                 reason = f"JSON {location} removed"
                 return True, reason
             else:
@@ -139,15 +139,23 @@ class DatabaseSystem(DatabaseBInterface):
     def disconnect(self):
         print("-Disconnecting from MYSQL Database")
 
-    def create(self, location: str, data: str) -> Tuple[bool, str]:
+    def create(self, location: str, data: str, test_db) -> Tuple[bool, str]:
         try:
             new_data = ImageStore(body=data)
-            self.db.add(new_data)
-            self.db.commit()
-            self.db.refresh(new_data)
-            reason = f"Data stored succesfully at {location}"
-            print(reason)
-            return (True, reason)
+            if test_db is None:
+                self.db.add(new_data)
+                self.db.commit()
+                self.db.refresh(new_data)
+                reason = f"Data stored succesfully at {location}"
+                print(reason)
+                return (True, reason)
+            else:
+                test_db.add(new_data)
+                test_db.commit()
+                test_db.refresh(new_data)
+                reason = f"Data stored succesfully at {location}"
+                print(reason)
+                return (True, reason)
         except Exception as e:
             reason = (
                 f"-Failed to create data in location {location}, reason: "
@@ -156,12 +164,19 @@ class DatabaseSystem(DatabaseBInterface):
             print(reason)
             return False, reason
 
-    def read(self, location: str) -> Tuple[bool, str]:
+    def read(self, location: str, test_db) -> Tuple[bool, str]:
         try:
-            new_data = self.db.query(ImageStore).all()
-            reason = f"Data from MYSQL database at {location}"
-            print(reason, new_data)
-            return (new_data, reason)
+            if test_db is None:
+                new_data = self.db.query(ImageStore).all()
+                reason = f"Data from MYSQL database at {location}"
+                print(reason, new_data)
+                return (True, reason, new_data)
+            else:
+                new_data = test_db.query(ImageStore).all()
+                reason = f"Data from MYSQL database at {location}"
+                print(reason, new_data)
+                return (True, reason, new_data)
+
         except Exception as e:
             reason = (
                 f"-Failed to create data in location {location}, reason: "
@@ -170,17 +185,30 @@ class DatabaseSystem(DatabaseBInterface):
             print(reason)
             return False, reason
 
-    def update(self, location: str, data: str) -> Tuple[bool, str]:
+    def update(self, location: str, data: str, test_db) -> Tuple[bool, str]:
         try:
-            new_data = self.db.query(ImageStore).filter(ImageStore.id == location).first()
-            new_data.body = data
-            self.db.commit()
-            if not new_data:
-                return {'detail': f'Data with the id {id} is not available'}
+            if test_db is None:
+                new_data = self.db.query(ImageStore).filter(ImageStore.id == location).first()
+                new_data.body = data
+                self.db.commit()
+                if not new_data:
+                    return {'detail': f'Data with the id {id} is not available'}
 
-            reason = f"Data updated from MYSQL database at {location}"
-            print(reason, new_data)
-            return (new_data, reason)
+                reason = f"Data updated from MYSQL database at {location}"
+                print(True, reason, new_data)
+                return (True, reason, new_data)
+
+            else:
+                new_data = test_db.query(ImageStore).filter(ImageStore.id == location).first()
+                new_data.body = data
+                test_db.commit()
+                if not new_data:
+                    return {'detail': f'Data with the id {id} is not available'}
+
+                reason = f"Data updated from MYSQL database at {location}"
+                print(reason, new_data)
+                return (True, reason, new_data)
+
         except Exception as e:
             reason = (
                 f"-Failed to create data in location {location}, reason: "
@@ -189,15 +217,25 @@ class DatabaseSystem(DatabaseBInterface):
             print(reason)
             return False, reason
 
-    def delete(self, location: str) -> Tuple[bool, str]:
+    def delete(self, location: str, test_db) -> Tuple[bool, str]:
         try:
-            new_data = self.db.query(ImageStore).filter(ImageStore.id == location).delete()
-            if not new_data:
-                return {'detail': f'Data with the id {id} is not available'}
+            if test_db is None:
+                new_data = self.db.query(ImageStore).filter(ImageStore.id == location).delete()
+                if not new_data:
+                    return {'detail': f'Data with the id {id} is not available'}
 
-            reason = f"Data with id {location} has been deleted from database"
-            print(reason, new_data)
-            return (new_data, reason)
+                reason = f"Data with id {location} has been deleted from database"
+                print(True, reason)
+                return (True, reason)
+            else:
+                new_data = test_db.query(ImageStore).filter(ImageStore.id == location).delete()
+                if not new_data:
+                    return {'detail': f'Data with the id {id} is not available'}
+
+                reason = f"Data with id {location} has been deleted from database"
+                print(reason, new_data)
+                return (True, reason)
+
         except Exception as e:
             reason = (
                 f"-Failed to create data in location {location}, reason: "
@@ -215,58 +253,64 @@ class PhoneBookSystem:
         self.db.connect()
         print("System startup completed")
 
-    def createContact(self, data: dict) -> Tuple[bool, str]:
+    def createContact(self, data: dict, test_db=None) -> Tuple[bool, str]:
         print("Creating contact")
 
         phone = data['phone']
 
         test = data['name']
 
-        created, reason = self.db.create(phone, test)
+        # created, reason = self.db.create(phone, data, test_db)
+        # for db
+        created, reason = self.db.create(phone, test, test_db)
 
         if not created:
             print(reason)
             return True, reason
+        return reason
 
-    def readContact(self, data: dict) -> Tuple[bool, str]:
+    def readContact(self, data: str, test_db=None) -> Tuple[bool, str, Dict[str, str]]:
         print("Reading from db")
 
-        phone = data['phone']
-        data, reason = self.db.read(phone)
+        phone = data
+        result, reason ,data= self.db.read(phone, test_db)
 
-        if not data:
+        if not result:
             print(reason)
             return True, reason
         
         print(data)
         return data
 
-    def updateContact(self, data: dict) -> Tuple[bool, str]:
+    def updateContact(self, data: dict, test_db=None) -> Tuple[bool, str]:
         print("Updating db")
 
         id = data["id"]
         phone = data['phone']
-        data, reason = self.db.update(id, phone)
+        # for db system
+        updated, reason, data = self.db.update(id, phone, test_db)
+        # for memory
+        # updated, reason = self.db.update(id, data, test_db)
 
-        if not data:
+        if not updated:
             print(reason)
             return True, reason
-        
+        # return reason
         print(data)
         return data
 
-    def deleteContact(self, data: dict) -> Tuple[bool, str]:
+    def deleteContact(self, data: dict, test_db=None) -> Tuple[bool, str]:
         print("Deleting contact from db")
 
         id = data["id"]
-        data, reason = self.db.delete(id)
+        data, reason = self.db.delete(id, test_db)
 
         if not data:
             print(reason)
             return True, reason
-        
-        print(data)
-        return data
+        return reason
+        # print(data)
+        # return data
 
 database_service = DatabaseSystem()
 # database_service = MemorySystem()
@@ -275,11 +319,15 @@ phone_book_system = PhoneBookSystem(database_service)
 phone_book_system.setUpSystem()
 
 id = 1
+# for memory system
+# id = "0788926713.json"
 name = "Person-1"
-phone = "0788926713"
-phone2 = "0782142404"
+phone = "0788926713.json"
+phone2 = "0782142404.json"
 # phone_book_system.createContact({"name": name, "phone": phone})
-# phone_book_system.readContact({"name": name, "phone": phone})
+# phone_book_system.readContact(phone)
 # phone_book_system.updateContact({"id": id, "phone": phone2})
-phone_book_system.deleteContact({"id": id})
+'''for memory system'''
+# phone_book_system.updateContact({"id": id, "phone": name})
+# phone_book_system.deleteContact({"id": id})
 
--- test.py
+++
@@ -1,6 +0,0 @@
-data = open('test.json', 'rb')
-import boto3
-
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
-s3.Bucket('image-store-1995').put_object(Key='test.json', Body=data)
